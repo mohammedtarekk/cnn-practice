7be7e789571e2a24f05fb58316d6c59ce1823216
mohammedtarekk <mohammedtarek74@yahoo.com>
2021-04-22 01:43:11 +0200

    Started working on the GUI

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import perceptron
-import GUI
 
-# GUI Creation
-GUI.create()
+################ LOAD DATA ###################
+data = pd.read_csv('IrisData.txt')
+
+# set global variables
+featuresNum = len(data.columns) - 1
+
+################ GUI Creation ################
+# parent window
+parent = tk.Tk()
+parent.geometry("700x500")
+parent.title("Perceptron")
+parent.resizable(0, 0)
+
+# Select 2 Features
+row = 0
+
+#for val in data.columns():
+  #  ttk.Radiobutton(parent, text=val, variable=v,
+  #      value = value).pack(side = TOP, ipady = 5)
+
+# Enter learning rate
+row = 3
+label = ttk.Label(parent, text="Enter learning rate:")
+label.grid(column=0, row=row, padx=10, pady=10)
+learningRate_txt = ttk.Entry(parent, width=5)
+learningRate_txt.grid(column=1, row=row, padx=10, pady=10)
+
+# Enter no. of epochs
+row = 4
+label = ttk.Label(parent, text="Enter no. of epochs:")
+label.grid(column=0, row=row, padx=10, pady=10)
+epochsNum_txt = ttk.Entry(parent, width=5)
+epochsNum_txt.grid(column=1, row=row, padx=10, pady=10)
+
+parent.mainloop()
 
 # Set Global Variables from GUI
 # ...
--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,9 @@
-def train():
-    print("train")
+def train(x_train, y_train, isBiased):
+    pass
 
+def test(x_test, y_test):
+    pass
 
-def test():
-    print("test")
+def evaluate():
+    # It should return the accuracy and show the confusion matrix
+    pass
